a082af144e674f2c8663656d613b8f3d9e0d859a
mybffjustn <juh369@g.harvard.edu>
2022-05-20 19:12:45 -0500

    Add cpp and 0.cpp

---
+++ cpp/0/0.cpp
@@ -0,0 +1,11 @@
+// Justin Hoang
+// 2022-MAY-20-FRI
+// Say "Hello, World!" With C++
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
