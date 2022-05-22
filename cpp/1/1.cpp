03f7c785b535b5ab71770ddb33b2c4f0619152ca
mybffjustn <juh369@g.harvard.edu>
2022-05-22 14:20:53 -0500

    Add 1.cpp

---
+++ cpp/1/1.cpp
@@ -0,0 +1,27 @@
+//
+// Justin Hoang
+// 2022-MAY-22-SUN
+// Input and Output
+//
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  int a;
+  int b;
+  int c;
+  int sum;
+
+  cin >> a;
+  cin >> b;
+  cin >> c;
+  sum = a + b + c;
+  cout << sum;
+
+  return 0;
+}
