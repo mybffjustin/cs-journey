f9050508539632b7e7eddd4e8c0e6f71c4d6cc72
mybffjustn <juh369@g.harvard.edu>
2022-06-04 13:26:42 -0500

    Add 8.c

---
+++ c/8/8.c
@@ -0,0 +1,41 @@
+//
+// Justin Hoang
+// 2022-JUN-04-SAT
+// Bitwise Operators
+//
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// Complete the following function.
+
+void calculate_the_maximum(int n, int k) {
+  // Write your code here.
+  int maxAnd = 0;
+  int maxOr = 0;
+  int maxXor = 0;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = i + 1; j <= n; j++) {
+      if (((i & j) > maxAnd) && ((i & j) < k)) {
+        maxAnd = i & j;
+      }
+      if (((i | j) > maxOr) && ((i | j) < k)) {
+        maxOr = i | j;
+      }
+      if (((i ^ j) > maxXor) && ((i ^ j) < k)) {
+        maxXor = i ^ j;
+      }
+    }
+  }
+  printf("%d\n%d\n%d\n", maxAnd, maxOr, maxXor);
+}
+
+int main() {
+  int n, k;
+
+  scanf("%d %d", &n, &k);
+  calculate_the_maximum(n, k);
+
+  return 0;
+}
