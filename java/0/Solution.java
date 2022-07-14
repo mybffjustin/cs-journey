0638c00d9847ab4f19d82cf115875291dd1181e1
mybffjustin <juh369@g.harvard.edu>
2022-07-13 20:27:02 -0500

    Add java 1

--- java/0/Solution.java
+++ java/0/Solution.java
@@ -1,11 +1,12 @@
 // Justin Hoang
 // 2022-JUL-4-MON
 // Welcome to Java!
+// Java 0
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
+  public static void main(String[] args) {
+    /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+  }
 }
---
+++ java/1/Solution.java
@@ -0,0 +1,20 @@
+// Justin Hoang
+// 2022-JUL-13-MON
+// Java Stdin and Stdout I
+// Java 1
+
+import java.util.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = scan.nextInt();
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
