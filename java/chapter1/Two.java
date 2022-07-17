34c78fb6e940d7eb27c0afcbc82ce6a0ebb96bef
mybffjustin <juh369@g.harvard.edu>
2022-07-17 14:50:37 -0500

    add exercise 1.2

---
+++ java/chapter1/Two.java
@@ -0,0 +1,17 @@
+// Justin Hoang
+// 2022-JUL-17-SUN
+// Chapter 1
+// Programming Exercise 1.2
+// (Display three messages) Write a program that displays
+// I love Java five times.
+
+public class Two {
+
+  public static void main(String args[]) {
+    int count = 0;
+    while (count < 5) {
+      System.out.println("I love Java");
+      ++count;
+    }
+  }
+}
