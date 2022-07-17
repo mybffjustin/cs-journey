b41287f218c452437db1164281da92f76f82ebb7
mybffjustin <juh369@g.harvard.edu>
2022-07-17 14:58:53 -0500

    add exercise 1.3

---
+++ java/chapter1/Three.java
@@ -0,0 +1,19 @@
+// Justin Hoang
+// 2022-JUL-17-SUN
+// Chapter 1
+// Programming Exercise *1.3
+// (Display a pattern) Write a program that displays the following pattern:
+//    J
+// J aaa v vaaa
+// J J aa v v a a
+// J aaaa v aaaa
+
+public class Three {
+
+  public static void main(String[] args) {
+    System.out.println("   J");
+    System.out.println("J aaa v vaaa");
+    System.out.println("J J aa v v a a");
+    System.out.println("J aaaa v aaaa");
+  }
+}
