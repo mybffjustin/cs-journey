5ea906f85bef9d947d54f24aa95a4594ebe8dd7c
mybffjustin <juh369@g.harvard.edu>
2022-07-17 14:41:36 -0500

    add exercise 1.1

---
+++ java/chapter1/One.java
@@ -0,0 +1,15 @@
+// Justin Hoang
+// 2022-JUL-17-SUN
+// Chapter 1
+// Programming Exercise 1.1
+// (Display three messages) Write a program that displays
+// Welcome to Java, Learning Java Now, and Programming is fun.
+
+public class One {
+
+  public static void main(String args[]) {
+    System.out.println("Welcome to Java");
+    System.out.println("Learning Java Now");
+    System.out.println("Programming is fun");
+  }
+}
