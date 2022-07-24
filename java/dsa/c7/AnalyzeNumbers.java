dc3d6ebd6d61c1f9e58ed9f2daafadc0e8d622da
mybffjustin <juh369@g.harvard.edu>
2022-07-24 18:01:36 -0500

    update AnalyzeNumbers.java

--- java/dsa/c7/AnalyzeNumbers.java
+++ java/dsa/c7/AnalyzeNumbers.java
@@ -15,19 +15,20 @@ public class AnalyzeNumbers {
 
     Scanner input = new Scanner(System.in);
     System.out.println("Enter the number of items: ");
-    n = input.nextInt();
-    numbers = new double[n];
+    n = input.nextInt(); // Prompt user to enter the array size
+    numbers = new double[n]; // Create array with user entered size
     sum = 0;
 
     System.out.print("Enter the numbers: ");
     for (i = 0; i < n; i++) {
-      numbers[i] = input.nextDouble();
-      sum += numbers[i];
+      numbers[i] = input.nextDouble(); // Reads input and stores numbers into the array
+      sum += numbers[i]; // Adds each number to sum
     }
 
-    average = sum / n;
+    average = sum / n; // Obtain the average
 
     count = 0; // The number of elements above average
+    // Compare each number in the array with the average to count the number of values above the average
     for (i = 0; i < n; i++) {
       if (numbers[i] > average) {
         count++;
