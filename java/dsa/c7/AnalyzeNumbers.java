f8ccf5282d581b3d1f2fb975e1fc048d661dfd65
mybffjustin <juh369@g.harvard.edu>
2022-07-24 17:47:18 -0500

    add AnalyzeNumbers.java

---
+++ java/dsa/c7/AnalyzeNumbers.java
@@ -0,0 +1,40 @@
+// Justin Hoang
+// 2022-JUL-24-SUN
+
+import java.util.*;
+
+public class AnalyzeNumbers {
+
+  public static void main(String[] args) {
+    int n;
+    double[] numbers;
+    double sum;
+    double average;
+    int count;
+    int i;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the number of items: ");
+    n = input.nextInt();
+    numbers = new double[n];
+    sum = 0;
+
+    System.out.print("Enter the numbers: ");
+    for (i = 0; i < n; i++) {
+      numbers[i] = input.nextDouble();
+      sum += numbers[i];
+    }
+
+    average = sum / n;
+
+    count = 0;
+    for (i = 0; i < n; i++) {
+      if (numbers[i] > average) {
+        count++;
+      }
+    }
+
+    System.out.println("Average is " + average);
+    System.out.println("Number of elements above the average is " + count);
+  }
+}
